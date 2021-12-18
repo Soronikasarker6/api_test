103c711394f853e35c54f7c08776f73ebedfb704
Lite Computer <soronika.sarker.cse@ulab.edu.bd>
2021-12-18 20:05:13 +0600

    name,location, code

--- lib/details.dart
+++ lib/details.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Details extends StatelessWidget {
-  const Details({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
