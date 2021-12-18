2d8765e1bc1d2a35dd4fa6298859cefc8205052b
Lite Computer <soronika.sarker.cse@ulab.edu.bd>
2021-12-18 20:02:29 +0600

    done

--- lib/fatch_data.dart
+++ lib/fatch_data.dart
@@ -1,6 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:api_test/get_card.dart';
 
 const url = 'http://50.21.176.24/tables#';
 class FatchDataScreen extends StatefulWidget {
@@ -11,32 +12,34 @@ class FatchDataScreen extends StatefulWidget {
 
 class _FatchDataScreenState extends State<FatchDataScreen> {
 
-   List<dynamic> user = [];
-   bool isLoading = true;
+  List users = [];
+  bool isLoading = false;
 
-
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.getData();
+  }
 
   void getData()async{
     http.Response response= await http.get(Uri.parse(url));
-    print(response.statusCode);
 
     if(response.statusCode== 200){
       String data = response.body;
-      print(data);
-      var decodeDate = jsonDecode(data);
-      var name = decodeDate['tables'][0]['name'];
-      var created = decodeDate['tables'][1]['created_at'];
-      print (created);
+      var decodeData = jsonDecode(data)['tables'];
       setState(() {
-        user = name;
+        users = decodeData;
       });
+      var name = decodeData['tables'][0]['name'];
+      var created = decodeData['tables'][1]['created_at'];
+      print (created);
       print(name);
-      return decodeDate;
+      return decodeData;
     }else{
       setState(() {
-        user = [];
+        users =[];
       });
-      print (response.statusCode);
     }
   }
   @override
@@ -45,46 +48,20 @@ class _FatchDataScreenState extends State<FatchDataScreen> {
     getData();
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('Api fetch'),
       ),
-    body: getBody(),
+      body: getBody(),
     );
   }
   Widget getBody(){
-    List items = ["1", "2"];
-    return ListView.builder( itemCount: user.length,itemBuilder: (context,index){
-      return getCard(user[index]);
-    },);
+    // List items = ["1", "2", "3"];
+    return ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index){
+      return getCard(users[index]);
+    });
   }
-  Widget getCard(items){
-    var  tables= items['id']['name']['location']['code']['waiter']['active']['outside']['created']['updated'];
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ListTile(
-          title: Row(
-            children: [
-              Container(
-                width: 40,
-                height: 40,
-                decoration: BoxDecoration(
-                  color:  Colors.white,
-                  borderRadius: BorderRadius.circular(10.0 ),
 
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children:<Widget> [
-                  Text(tables.toString(),),
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 
 }
-//http://50.21.176.24/tables#
